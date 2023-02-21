fcf5d103e0c0a33e49baf2aca83ac15cc2b8b7c2
Naomi <Learn.naomi@gmail.com>
2023-02-20 16:10:10 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
  * Return: Always 1.
-* /
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", 58);
 	return (1);
 }
 
